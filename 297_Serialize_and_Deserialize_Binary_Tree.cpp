0c41583028be6f70ea1830d0e91cfff1ad722a6d
Xiaodong Liu <71720210+1042976@users.noreply.github.com>
2022-05-04 23:25:08 -0400

    Update 297_Serialize_and_Deserialize_Binary_Tree.cpp

--- 297_Serialize_and_Deserialize_Binary_Tree.cpp
+++ 297_Serialize_and_Deserialize_Binary_Tree.cpp
@@ -12,60 +12,98 @@ public:
 
     // Encodes a tree to a single string.
     //T-O(2^n) S-O(2^n), bfs
-    string serialize(TreeNode* root) {
-        //--->1-->2 3-->null null 4 5--->1#2#3# # #4#5# # #
-        queue<TreeNode*> Q;
-        string res = "";
-        Q.push(root);
-        while(!Q.empty()){
-            TreeNode* cur = Q.front();
-            Q.pop();
-            if(cur != nullptr){
-                res.append(to_string(cur->val));
-                Q.push(cur->left);
-                Q.push(cur->right);
-            }
-            res.push_back('#');
+//     string serialize(TreeNode* root) {
+//         //--->1-->2 3-->null null 4 5--->1#2#3###4#5###
+//         queue<TreeNode*> Q;
+//         string res = "";
+//         Q.push(root);
+//         while(!Q.empty()){
+//             TreeNode* cur = Q.front();
+//             Q.pop();
+//             if(cur != nullptr){
+//                 res.append(to_string(cur->val));
+//                 Q.push(cur->left);
+//                 Q.push(cur->right);
+//             }
+//             res.push_back('#');
+//         }
+//         return res;
+//     }
+
+//     // Decodes your encoded data to tree.
+//     //T-O(K), S-O(K) 
+//     TreeNode* deserialize(string data) {
+//         //#1#2#3###4#5##--->1-->2 3-->null null 4 5
+//         TreeNode* root = new TreeNode(0);
+//         int leftIdx = 0;
+//         int rightIdx = 0;
+//         if(data[0] == '#'){
+//             return nullptr;
+//         }else{
+//             rightIdx = data.find('#', 0);
+//             root->val = stoi(data.substr(0, rightIdx));
+//         }
+//         ++rightIdx;
+//         leftIdx = rightIdx;
+//         queue<TreeNode*> Q;
+//         Q.push(root);
+//         while(!Q.empty()){
+//             TreeNode* cur = Q.front();
+//             Q.pop();
+//             rightIdx = data.find('#', rightIdx);
+//             if(leftIdx != rightIdx){
+//                 cur->left = new TreeNode(stoi(data.substr(leftIdx, rightIdx)));
+//                 Q.push(cur->left);
+//             }
+//             ++rightIdx;
+//             leftIdx = rightIdx;
+//             rightIdx = data.find('#', rightIdx);
+//             if(leftIdx != rightIdx){
+//                 cur->right = new TreeNode(stoi(data.substr(leftIdx, rightIdx)));
+//                 Q.push(cur->right);
+//             }
+//             ++rightIdx;
+//             leftIdx = rightIdx;
+//         }
+//         return root;
+//     }
+    //T-O(K), S-O(K)
+    void rserialize(TreeNode* root, string& str){
+        if(root != nullptr){
+            str.append(to_string(root->val)+",");
+        }else{
+            str.append("n,");
+            return;
         }
+        rserialize(root->left, str);
+        rserialize(root->right, str);
+    }
+    
+    string serialize(TreeNode* root){
+        string res = "";
+        rserialize(root, res);
         return res;
     }
-
-    // Decodes your encoded data to tree.
-    //T-O(K), S-O(K) 
-    TreeNode* deserialize(string data) {
-        //#1#2#3# # #4#5# # --->1-->2 3-->null null 4 5
-        TreeNode* root = new TreeNode(0);
-        int leftIdx = 0;
-        int rightIdx = 0;
-        if(data[0] == '#'){
-            return nullptr;
+    
+    //T-O(K), S-O(K)
+    TreeNode* rdeserialize(string data, int& idx){
+        if(data[idx] != 'n'){
+            int idxComma = data.find(',', idx);
+            int val = stoi(data.substr(idx, idxComma));
+            TreeNode* node = new TreeNode(val);
+            idx = idxComma+1;
+            node->left = rdeserialize(data, idx);
+            node->right = rdeserialize(data, idx);
+            return node;
         }else{
-            rightIdx = data.find('#', 0);
-            root->val = stoi(data.substr(0, rightIdx));
+            idx += 2;
+            return nullptr;
         }
-        ++rightIdx;
-        leftIdx = rightIdx;
-        queue<TreeNode*> Q;
-        Q.push(root);
-        while(!Q.empty()){
-            TreeNode* cur = Q.front();
-            Q.pop();
-            rightIdx = data.find('#', rightIdx);
-            if(leftIdx != rightIdx){
-                cur->left = new TreeNode(stoi(data.substr(leftIdx, rightIdx)));
-                Q.push(cur->left);
-            }
-            ++rightIdx;
-            leftIdx = rightIdx;
-            rightIdx = data.find('#', rightIdx);
-            if(leftIdx != rightIdx){
-                cur->right = new TreeNode(stoi(data.substr(leftIdx, rightIdx)));
-                Q.push(cur->right);
-            }
-            ++rightIdx;
-            leftIdx = rightIdx;
-        }
-        return root;
+    }
+    
+    TreeNode* deserialize(string data){
+        int idx = 0;
+        return rdeserialize(data, idx);
     }
     
 };
