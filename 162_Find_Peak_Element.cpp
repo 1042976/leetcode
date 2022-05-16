36affeb21a09851ca37ba2e99ef083c84f0a9f5c
Xiaodong Liu <71720210+1042976@users.noreply.github.com>
2022-05-15 23:25:52 -0400

    Create 162_Find_Peak_Element.cpp

---
+++ 162_Find_Peak_Element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    // Time O(log_2(n)), Space O(1)
+    int findPeakElement(vector<int>& nums) {
+        int l = 0, r = nums.size()-1;
+        while(l < r){
+            int m = l+((r-l)>>1);
+            if(nums[m] < nums[m+1]){
+                l = m+1;
+            }else{
+                r = m;
+            }
+        }
+        return l;
+    }
+};
