e1380c90ce86dfa70adc7a09e81a17f97e3ad04c
Xiaodong Liu <71720210+1042976@users.noreply.github.com>
2022-05-21 15:14:04 -0400

    Create 0986_Interval_List_Intersections.cpp

---
+++ 0986_Interval_List_Intersections.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+        int i = 0, j = 0;
+        vector<vector<int> > res;
+        while(i < firstList.size() && j < secondList.size()){
+            int l = max(firstList[i][0], secondList[j][0]);
+            int r = min(firstList[i][1], secondList[j][1]);
+            if(l <= r){
+                res.emplace_back(vector<int>{l, r});
+            }
+            firstList[i][1] < secondList[j][1]? ++i : ++j;
+        }
+        return res;
+    }
+};
