e81523b368be9b6e0cab23b72fb56d4b232bce97
Xiaodong Liu <71720210+1042976@users.noreply.github.com>
2022-06-24 12:27:39 -0400

    Create 0279_Perfect_Squares.cpp

---
+++ 0279_Perfect_Squares.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    // int minCount = INT_MAX;  
+    // void dfs(int n, int largest, int count){
+    //     if(++count > minCount){
+    //         return;
+    //     }
+    //     for(int i = largest; i >= 1; --i){
+    //         int rest = n-i*i;
+    //         if(rest == 0){
+    //             minCount = min(minCount, count);
+    //         }else if(rest > 0){
+    //             dfs(rest, i, count);
+    //         }
+    //     }
+    // }
+    // //Time O(N * sqrt(N)), Space O(N)
+    // int numSquares(int n) {
+    //     dfs(n, sqrt(n), 0);
+    //     return minCount;
+    // }
+    bool isSquare(int n){
+        int a = sqrt(n);
+        return n == a*a;
+    }
+    int numSquares(int n){
+        while(n%4 == 0){
+            n /= 4;
+        }
+        if(n%8 == 7){
+            return 4;
+        }
+        int a = sqrt(n);
+        if(a*a == n){
+            return 1;
+        }
+        for(int i = 1; i <= a; ++i){
+            if(isSquare(n-i*i)){
+                return 2;
+            }
+        }
+        return 3;
+    }
+};
