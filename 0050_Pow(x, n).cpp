0a54ed3732d09629ad4e1aed5b9099273d63f7ef
Xiaodong Liu <71720210+1042976@users.noreply.github.com>
2022-07-13 18:12:48 -0400

    Update 0050_Pow(x, n).cpp

--- 0050_Pow(x, n).cpp
+++ 0050_Pow(x, n).cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
-    //Time O(logn) Space O(1)
-    double myPow(double x, int n) {
-        if(n == 0){
-            return 1;
+    // double fastPow(double x, long long n){
+    //     double half = pow(x, n/2);
+    //     if(n%2){
+    //         return x*half*half;
+    //     }else{
+    //         return half*half;
+    //     }
+    // }
+    // //Time O(logn), Space O(logn)
+    // double myPow(double x, int n) {
+    //     if(n < 0){
+    //         long long m = n;
+    //         return fastPow(1/x, -m);
+    //     }else{
+    //         return fastPow(x, n);
+    //     }
+    // }
+    
+    //Time O(logn), Space O(1)
+    double myPow(double x, int n){
+        long long m = n;
+        if(m < 0){
+            x = 1/x;
+            m = -m;
         }
-        x = n < 0? 1.0/x : x;
-        double tmp = x, res = x;
-        n = n < 0? abs(n+1) : n-1;
-        int a = 1;
-        while(n > 0){
-            res *= tmp;
-            n -= a;
-            if(a <= (n>>1)){
-                a *= 2;
-                tmp *= tmp;
-            }else{
-                a = 1;
-                tmp = x;
+        long long i = m;
+        double res = 1;
+        while(i > 0){
+            if(i%2){
+                res *= x;
             }
+            i >>= 1;
+            x = x*x;
         }
         return res;
-        
     }
 };
