552c10b6479dad40946ff95a29c89d4e3186c207
Xiaodong Liu <71720210+1042976@users.noreply.github.com>
2022-07-25 15:52:20 -0400

    Update 1044_Longest_Duplicate_Substring.cpp

--- 1044_Longest_Duplicate_Substring.cpp
+++ 1044_Longest_Duplicate_Substring.cpp
@@ -3,16 +3,24 @@ public:
     int mod = 1e+9+7;
     int a = 26;
     int N;
+    long long getPower(int a, int b){
+        long long res = 1;
+        while(b != 0){
+            if(b%2){
+                res = res*a%mod;
+            }
+            a = (long long)a*a%mod;
+            b >>= 1;
+        }
+        return res;
+    }
     int search(string& s, int L){
         unordered_map<int, vector<int> > hashMap;
         long long hashVal = 0;
         for(int i = 0; i < L; ++i){
             hashVal = ((hashVal*a) + (s[i]-'a'))%mod;
         }
-        long long aExp = 1;
-        for(int i = 1; i < L; ++i){
-            aExp = (aExp*a)%mod;
-        }
+        long long aExp = getPower(a, L-1);
         hashMap[hashVal].emplace_back(0);
         for(int i = 1; i < N-L+1; ++i){
             hashVal = ((hashVal-(s[i-1]-'a')*aExp)*a%mod+mod)%mod + (s[i+L-1]-'a');
@@ -44,6 +52,7 @@ public:
         return startPos == -1? "" : s.substr(startPos, left);
     }
     
+    //Time O(LNlogN), Space O(N)
     // string longestDupSubstring(string s){
     //     int len = s.length();
     //     int left = 1, right = len-1;
@@ -66,6 +75,7 @@ public:
     //         }else{
     //             right = mid-1;
     //         }
+    //         hashSet.clear();
     //     }
     //     return string(longest);
     // }
